186ff5ccf24310bdfe106e820db8d819f5855603
Douglas Eduardo Oliveira da Rosa <oliveira.douglasedu@outlook.com>
2020-10-28 19:27:26 -0300

    new begin screen

--- src/sections/Begin/index.jsx
+++ src/sections/Begin/index.jsx
@@ -8,20 +8,21 @@ export default function Begin() {
   return (
     <div className="begin-container">
       <div className="begin-info-container">
-        <img className="logo-icon" alt="Logo" src={logo} />
         <div>
-          <span className="begin-title">
-            Digital Legacy System
-          </span>
+          <img className="logo-icon" alt="Logo" src={logo} />
+          <div>
+            <span className="begin-title">
+              Digital Legacy System
+            </span>
 
-          <span className="begin-subtitle">
-            Hello! This portfolio can help you understand about our project
-          </span>
-
-          <span className="begin-authors">
-            Douglas Oliveira && Lucas Rohr, 2020-21
-          </span>
+            <span className="begin-subtitle">
+              Welcome to a nice introduction to our project
+            </span>
+          </div>
         </div>
+        <span className="begin-authors">
+          Douglas Oliveira && Lucas Rohr, 2020-21
+        </span>
       </div>
     </div>
   );
